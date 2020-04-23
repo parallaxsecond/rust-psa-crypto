971bff1466bbe3af2e4b4f290d7bb34bf49c2aec
Hugues de Valon <hugues.devalon@arm.com>
2020-04-23 16:13:51 +0100

    Add the base repository
    Copy from what we do on our other Rust crates:
    * rustc lints
    * CI set up
    * LICENSE
    * README file
    
    Signed-off-by: Hugues de Valon <hugues.devalon@arm.com>

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# rust-psa-crypto
-Rust wrapper around the PSA Cryptography API
+<!--
+  -- Copyright 2020 Contributors to the Parsec project. 
+  -- SPDX-License-Identifier: Apache-2.0
+--->
+# PSA Cryptography API Rust Wrapper
+
+This repository contains a Rust wrapper around the [PSA Cryptography API](https://developer.arm.com/architectures/security-architectures/platform-security-architecture/documentation).
+
+## License
+
+The software is provided under Apache-2.0. Contributions to this project are accepted under the same license.
+
+## Contributing
+
+Please check the [**Contribution Guidelines**](https://parallaxsecond.github.io/parsec-book/contributing.html)
+to know more about the contribution process.
---
+++ src/lib.rs
@@ -0,0 +1,46 @@
+// Copyright 2020 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+
+//! # PSA Cryptography API Wrapper
+//!
+//! This crate provides abstraction over an implementation of the PSA Cryptography API.
+//! You can find the API
+//! [here](https://developer.arm.com/architectures/security-architectures/platform-security-architecture/documentation).
+
+#![deny(
+    nonstandard_style,
+    const_err,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    private_in_public,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true,
+    missing_debug_implementations,
+    missing_docs,
+    trivial_casts,
+    trivial_numeric_casts,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications,
+    unused_results,
+    missing_copy_implementations
+)]
+// This one is hard to avoid.
+#![allow(clippy::multiple_crate_versions)]
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
