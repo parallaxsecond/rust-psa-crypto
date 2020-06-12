d361726eb544d70900fead93d79710f299bb75af
Samuel Bailey <samuel.bailey@arm.com>
2020-06-12 12:31:54 +0100

    Changed error handling when psa-crypto-sys operation fails - prioritise key handle closing error.
    Signed-off-by: Samuel Bailey <samuel.bailey@arm.com>

--- psa-crypto/src/operations/key_management.rs
+++ psa-crypto/src/operations/key_management.rs
@@ -7,6 +7,8 @@ use crate::initialized;
 use crate::types::key::{Attributes, Id, Lifetime};
 use crate::types::status::{Result, Status};
 use core::convert::TryFrom;
+use psa_crypto_sys::{psa_key_handle_t, psa_key_id_t};
+
 /// Generate a key or a key pair
 ///
 /// `id` can be set to `None` when creating a volatile key. Setting the `id` to something will
@@ -55,25 +57,13 @@ pub fn generate(attributes: Attributes, id: Option<u32>) -> Result<Id> {
         0
     };
     let mut handle = 0;
-    Status::from(unsafe { psa_crypto_sys::psa_generate_key(&key_attributes, &mut handle) })
-        .to_result()?;
+    let gen_res = Status::from(unsafe { psa_crypto_sys::psa_generate_key(&key_attributes, &mut handle) })
+        .to_result();
     Attributes::reset(&mut key_attributes);
-    match attributes.lifetime {
-        Lifetime::Persistent | Lifetime::Custom(_) => {
-            Status::from(unsafe { psa_crypto_sys::psa_close_key(handle) }).to_result()?;
-            Ok(Id {
-                id,
-                handle: None
-            })
-        },
-        Lifetime::Volatile => {
-            Ok(Id {
-                id,
-                handle: Some(handle),
-            })
-        }
-    }
+
+    complete_new_key_operation(attributes.lifetime, id, handle, gen_res)
 }
+
 
 /// Destroy a key
 ///
@@ -181,28 +171,14 @@ pub fn import(attributes: Attributes, id: Option<u32>, data: &[u8]) -> Result<Id
     };
     let mut handle = 0;
 
-    Status::from(unsafe {
+    let gen_res = Status::from(unsafe {
         psa_crypto_sys::psa_import_key(&key_attributes, data.as_ptr(), data.len(), &mut handle)
     })
-    .to_result()?;
+    .to_result();
 
     Attributes::reset(&mut key_attributes);
 
-    match attributes.lifetime {
-        Lifetime::Persistent | Lifetime::Custom(_) => {
-            Status::from(unsafe { psa_crypto_sys::psa_close_key(handle) }).to_result()?;
-            Ok(Id {
-                id,
-                handle: None,
-            })
-        },
-        Lifetime::Volatile => {
-            Ok(Id {
-                id,
-                handle: Some(handle),
-            })
-        }
-    }
+    complete_new_key_operation(attributes.lifetime, id, handle, gen_res)
 }
 
 /// Export a public key or the public part of a key pair in binary format
@@ -244,18 +220,16 @@ pub fn export_public(key: Id, data: &mut [u8]) -> Result<usize> {
     let handle = key.handle()?;
     let mut data_length = 0;
 
-    Status::from(unsafe {
+    let export_res = Status::from(unsafe {
         psa_crypto_sys::psa_export_public_key(
             handle,
             data.as_mut_ptr(),
             data.len(),
             &mut data_length,
         )
-    })
-    .to_result()?;
-
+    }).to_result();
     key.close_handle(handle)?;
-
+    export_res?;
     Ok(data_length)
 }
 
@@ -296,7 +270,26 @@ pub fn get_key_attributes(key: Id) -> Result<Attributes> {
     initialized()?;
     let mut key_attributes = unsafe { psa_crypto_sys::psa_key_attributes_init() };
     let handle = key.handle()?;
-    let _get_attributes_status = Status::from( unsafe { psa_crypto_sys::psa_get_key_attributes(handle.into(), &mut key_attributes) } );
+    let attributes_res = Status::from( unsafe { psa_crypto_sys::psa_get_key_attributes(
+        handle.into(), &mut key_attributes) } ).to_result();
     key.close_handle(handle)?;
+    attributes_res?;
     Ok(Attributes::try_from(key_attributes)?)
+}
+
+
+/// Completes a new key operation (either generate or import)
+///
+/// If key is not `Volatile` (`Persistent` or `Custom(u32)`), handle is closed.
+///
+/// If a key is `Volatile`, `Id` returned contains the key `handle`. Otherwise, it does not.
+fn complete_new_key_operation(key_lifetime: Lifetime, id: psa_key_id_t, handle: psa_key_handle_t, operation_result: Result<()>) -> Result<Id> {
+    if key_lifetime != Lifetime::Volatile {
+        Status::from(unsafe { psa_crypto_sys::psa_close_key(handle) }).to_result()?;
+    }
+    operation_result?;
+    Ok(Id {
+        id,
+        handle: if key_lifetime == Lifetime::Volatile { Some(handle) } else { None }
+    })
 }
---
+++ psa-crypto/tests/mod.rs
@@ -0,0 +1,114 @@
+// Copyright 2020 Contributors to the Parsec project.
+// SPDX-License-Identifier: Apache-2.0
+
+
+use psa_crypto::types::key::{Attributes, Lifetime, Type, Policy, UsageFlags};
+use psa_crypto::types::algorithm::{Algorithm, Hash, AsymmetricSignature};
+
+#[test]
+fn generate_integration_test() {
+    let attributes = Attributes {
+        lifetime: Lifetime::Persistent,
+        key_type: Type::RsaKeyPair,
+        bits: 1024,
+        policy: Policy {
+            usage_flags: UsageFlags {
+                sign_hash: true,
+                verify_hash: true,
+                sign_message: true,
+                verify_message: true,
+                export: true,
+                encrypt: false,
+                decrypt: false,
+                cache: false,
+                copy: false,
+                derive: false,
+            },
+            permitted_algorithms: Algorithm::AsymmetricSignature(
+                AsymmetricSignature::RsaPkcs1v15Sign {
+                    hash_alg: Hash::Sha256.into(),
+                },
+            ),
+        },
+    };
+    let mut test_client = test_tools::TestClient::new();
+
+    // Ensure that a large number of keys can be generated
+    for key_index in 1..101u32 {
+        test_client.generate(attributes, key_index);
+    }
+}
+
+#[test]
+fn import_integration_test() {
+    const KEY_DATA: [u8; 140] = [
+    48, 129, 137, 2, 129, 129, 0, 153, 165, 220, 135, 89, 101, 254, 229, 28, 33, 138, 247, 20, 102,
+    253, 217, 247, 246, 142, 107, 51, 40, 179, 149, 45, 117, 254, 236, 161, 109, 16, 81, 135, 72,
+    112, 132, 150, 175, 128, 173, 182, 122, 227, 214, 196, 130, 54, 239, 93, 5, 203, 185, 233, 61,
+    159, 156, 7, 161, 87, 48, 234, 105, 161, 108, 215, 211, 150, 168, 156, 212, 6, 63, 81, 24, 101,
+    72, 160, 97, 243, 142, 86, 10, 160, 122, 8, 228, 178, 252, 35, 209, 222, 228, 16, 143, 99, 143,
+    146, 241, 186, 187, 22, 209, 86, 141, 24, 159, 12, 146, 44, 111, 254, 183, 54, 229, 109, 28,
+    39, 22, 141, 173, 85, 26, 58, 9, 128, 27, 57, 131, 2, 3, 1, 0, 1,
+    ];
+
+    let attributes = Attributes {
+        lifetime: Lifetime::Persistent,
+        key_type: Type::RsaPublicKey,
+        bits: 1024,
+        policy: Policy {
+            usage_flags: UsageFlags {
+                sign_hash: true,
+                sign_message: true,
+                verify_hash: true,
+                verify_message: true,
+                ..Default::default()
+            },
+            permitted_algorithms: AsymmetricSignature::RsaPkcs1v15Sign {
+                hash_alg: Hash::Sha256.into(),
+            }.into(),
+        },
+    };
+
+    let mut test_client = test_tools::TestClient::new();
+
+    // Ensure that a large number of keys can be imported
+    for key_index in 1..101u32 {
+        test_client.import(attributes, key_index, &KEY_DATA);
+    }
+
+
+}
+
+mod test_tools {
+    use psa_crypto::types::key::{Attributes, Id};
+    use psa_crypto::operations::key_management;
+
+    pub struct TestClient {
+        keys: Vec<Id>
+    }
+
+    impl TestClient {
+        pub fn new() -> Self {
+            psa_crypto::init().unwrap();
+            TestClient {
+                keys: Vec::new(),
+            }
+        }
+
+        pub fn generate(&mut self, attributes: Attributes, key_id: u32) {
+            self.keys.push(key_management::generate(attributes, Some(key_id)).unwrap());
+        }
+
+        pub fn import(&mut self, attributes: Attributes, key_id: u32, key_data: &[u8]) {
+            self.keys.push(key_management::import(attributes, Some(key_id), key_data).unwrap());
+        }
+    }
+
+    impl Drop for TestClient {
+        fn drop(&mut self) {
+            for key in self.keys.clone() {
+                unsafe { key_management::destroy(key) }.unwrap();
+            }
+        }
+    }
+}
