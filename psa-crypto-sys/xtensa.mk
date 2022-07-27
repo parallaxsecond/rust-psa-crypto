dd28b72af558fd5da7aa1a3a217c9f74cc0dc7c7
j-devel <38098238+j-devel@users.noreply.github.com>
2022-07-27 15:44:40 +0700

    xtensa.mk: Add `patch-entropy-poll` sub target

--- psa-crypto-sys/xtensa.mk
+++ psa-crypto-sys/xtensa.mk
@@ -1,13 +1,19 @@
 all: psa-xtensa
 
-psa-xtensa: patch-config-xtensa patch-x509-crt patch-cipher-info-from-string-xtensa
-	make -j CC=$(XTENSA_GCC) CFLAGS="-O2 -DMBEDTLS_NO_PLATFORM_ENTROPY=1 -DMBEDTLS_USE_PSA_CRYPTO=1" lib
+psa-xtensa: patch-config-xtensa patch-entropy-poll patch-x509-crt patch-cipher-info-from-string-xtensa
+	make -j CC=$(XTENSA_GCC) CFLAGS="-O2 -DMBEDTLS_USE_PSA_CRYPTO=1" lib
 
 CONFIG_H_FILE := include/mbedtls/mbedtls_config.h
 patch-config-xtensa:
 	sed -i -E 's/^(#define MBEDTLS_NET_C)/\/\/\0/g' $(CONFIG_H_FILE)
 	sed -i -E 's/^(#define MBEDTLS_TIMING_C)/\/\/\0/g' $(CONFIG_H_FILE)
 	sed -i -E 's/^(#define MBEDTLS_HAVE_TIME_DATE)/\/\/\0/g' $(CONFIG_H_FILE)
+
+# TODO connect to the platform's entropy source
+ENTROPY_POLL_C_FILE := library/entropy_poll.c
+patch-entropy-poll:
+	sed -i -E 's/^(int mbedtls_platform_entropy_poll)/\0__dummy/g' $(ENTROPY_POLL_C_FILE)
+	sed -i -E 's/^(#error "Platform entropy sources only work on).*/  int mbedtls_platform_entropy_poll\( void \*data, unsigned char \*output, size_t len, size_t \*olen \) \{ for \(int i = 0; i < len; i++\) \{ output[i] = 42; \}  \*olen = len;  return\( 0 \); \}/g' $(ENTROPY_POLL_C_FILE)
 
 X509_CRT_C_FILE := library/x509_crt.c
 patch-x509-crt:
